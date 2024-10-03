88b48aa70cb101c7cd5808cc89bd04cfd0c9f6da
Kamal Lahloh <kamal.lahloh@gmail.com>
2024-10-03 11:17:01 +0300

    ConnectionStrings

--- SuperHeroAPI-DotNet8/obj/Debug/net8.0/SuperHeroAPI-DotNet8.AssemblyInfo.cs
+++ SuperHeroAPI-DotNet8/obj/Debug/net8.0/SuperHeroAPI-DotNet8.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SuperHeroAPI-DotNet8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c95afae6f4de5f719dc4102b6dac355bdbd2ce44")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13c03945ede729b0b025c085a96950a8714b2eb5")]
 [assembly: System.Reflection.AssemblyProductAttribute("SuperHeroAPI-DotNet8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SuperHeroAPI-DotNet8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
